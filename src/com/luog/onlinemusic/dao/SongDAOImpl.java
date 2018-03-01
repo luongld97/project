f923e6253a61b94f7c8a13055ba1d4ad99916139
Your Name <you@example.com>
2018-03-01 19:24:49 +0700

    clgn

--- src/com/luog/onlinemusic/dao/SongDAOImpl.java
+++ src/com/luog/onlinemusic/dao/SongDAOImpl.java
@@ -153,11 +153,7 @@ public class SongDAOImpl implements SongDAO {
 					.createQuery("SELECT si.id as singerId, si.name as singerName, si.photo as singerPhoto, "
 							+ "so.id as id, so.name as name, " + "so.link as link, "
 							+ "so.lyric as lyric, " + "so.video as isVideo " + "FROM Singer si, Song so, SongDetail sd "
-							+ "WHERE sd.singer = si AND sd.song = so " + "ORDER BY so.id DESC");
-			query = session.createQuery("SELECT si.id as singerId, si.name as singerName, si.photo as singerPhoto, "
-					+ "so.id as songId, so.name as songName, " + "so.link as linkSong, " + "so.lyric as lyricSong, "
-					+ "so.video as songVideo " + "FROM Singer si, Song so, SongDetail sd "
-					+ "WHERE sd.singer = si AND sd.song = so " + "ORDER BY so.id DESC")
+							+ "WHERE sd.singer = si AND sd.song = so " + "ORDER BY so.id DESC")
 					.setResultTransformer(Transformers.aliasToBean(SongInfo.class));
 			songInfos = query.list();
 			transaction.commit();
